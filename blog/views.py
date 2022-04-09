552a2a14a9579064f0de06edbbc36de57b4e249b
hiwa2719 <hiahmadyan@gmail.com>
2022-04-09 18:37:37 +0430

    added PostListView & it's url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.utils import timezone
+from .models import Post, Comment
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
