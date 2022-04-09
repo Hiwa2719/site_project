aa2475412bd78469d5d5e4072c37871b1eee0f6f
hiwa2719 <hiahmadyan@gmail.com>
2022-04-09 17:48:16 +0430

    added post model and add blog to installed_apps

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True, auto_now=False)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.author} --> {self.title}'
+
+    def get_absolute_url(self):
+        return reverse('post:detail', kwargs={'pk': self.pk})
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def unpublish(self):
+        self.published_date = None
+        self.save()
