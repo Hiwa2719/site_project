fb9ae2a94928bd428afb3a953829509e24de5abc
hiwa2719 <hiahmadyan@gmail.com>
2022-04-09 19:18:52 +0430

    added PostDetailView and it's url

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView().as_view(), name='index'),
+    path('detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.utils import timezone
 from .models import Post, Comment
+from django.shortcuts import get_object_or_404
 
 
 class PostListView(ListView):
     queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+
+class PostDetailView(DetailView):
+    def get_object(self, queryset=None):
+        if self.request.user.is_authenticated:
+            return get_object_or_404(Post, pk=self.kwargs.get('pk'))
+        return get_object_or_404(Post.objects.filter(published_date__lte=timezone.now()), pk=self.kwargs.get('pk'))
