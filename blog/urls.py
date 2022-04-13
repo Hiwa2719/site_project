ee283e316f63dd7956633bcede9156434b46183d
hiwa2719 <hiahmadyan@gmail.com>
2022-04-13 16:48:58 +0430

    added PostCreateView

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'blog'
 
-
 urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
-    path('detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/', include([
+        path('detail/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+        path('create/', views.PostCreateView.as_view(), name='post_create')
+    ])),
     path('about/', views.AboutView.as_view(), name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView, CreateView, TemplateView
 from django.utils import timezone
 from .models import Post, Comment
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class PostListView(ListView):
@@ -16,8 +17,15 @@ class PostDetailView(DetailView):
         return get_object_or_404(Post.objects.filter(published_date__lte=timezone.now()), pk=self.kwargs.get('pk'))
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
+    fields = 'title', 'text'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        self.object.save()
+        return super().form_valid(form)
 
 
 class AboutView(TemplateView):
